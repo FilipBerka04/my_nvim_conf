c26ad0896fc7a44861db98eefca8e7a71e7eac9d
FilipBerka04 <filipberka4@gmail.com>
2026-04-29 18:54:48 +0200

    Managed to get tree-sitter to work

--- init.lua
+++ init.lua
@@ -3,15 +3,16 @@ require("config.lazy")
 vim.o.number = true
 vim.o.relativenumber = true
 vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
 
 -- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
 -- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
--- See `:help 'clipboard'`
-vim.api.nvim_create_autocmd("UIEnter", {
-  callback = function()
-    vim.o.clipboard = "unnamedplus"
-  end,
-})
+-- See `:help 'clipboard'` 
+--vim.api.nvim_create_autocmd("UIEnter", {
+--  callback = function()
+--    vim.o.clipboard = "unnamedplus"
+--  end,
+--})
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
